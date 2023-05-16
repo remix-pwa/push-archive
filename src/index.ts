9be4e41521f3a09c12243647b5894813aeb615ce
ShafSpecs <abbadshaf05@gmail.com>
2023-05-16 15:58:29 +0100

    new `push` exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 // client
 export { subscribeToPush, unsubscribeFromPush } from './client/subscription';
 
+// push 
+export { Push } from './push/strategy'
+
 // server
 export { sendNotifications } from './server/server';
 export type { sendNotificationParams } from './server/server';
 
 // types
-export type { NotificationObject, VapidDetails } from './types';
+export type { NotificationObject, VapidDetails, PushHandlerEnv, PushHandlerPlugin } from './types';
