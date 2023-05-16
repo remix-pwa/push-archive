7c9a36a284490234045708def590ee2869b31501
ShafSpecs <abbadshaf05@gmail.com>
2023-05-16 15:53:10 +0100

    main: push strategy :+1:

---
+++ src/push/strategy.ts
@@ -0,0 +1,23 @@
+import { PushHandlerPlugin, PushHandlerEnv } from "../types";
+
+export abstract class Push {
+  protected plugins: PushHandlerPlugin[];
+
+  constructor(plugins: PushHandlerPlugin[] = []) {
+    this.plugins = plugins;
+  }
+
+  protected async applyPlugins(
+    pluginMethod: keyof PushHandlerPlugin,
+    args: PushHandlerEnv
+  ) {
+    const promises = this.plugins.map(async (plugin) => {
+      if (plugin[pluginMethod]) {
+        await plugin[pluginMethod]!(args);
+      }
+    });
+    await Promise.all(promises);
+  }
+
+  abstract handlePush(event: PushEvent): Promise<void>;
+}
