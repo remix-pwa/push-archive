81a877a609b71b6048d4e25715dee2fdd504d1fd
ShafSpecs <abbadshaf05@gmail.com>
2023-05-16 15:37:16 +0100

    global typings implemented and exported

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,21 @@
 /**
  * @fileoverview Types used across the package. To be re-exported from `index.ts`
  */
+
+/**
+ * 
+ */
+export type VapidDetails = {
+  publicKey: string,
+  privateKey: string,
+  subject: string,
+};
+
+export interface NotificationObjectOptions extends NotificationOptions {
+  body: string
+}
+
+export type NotificationObject = {
+  title: string;
+  options: NotificationObjectOptions
+}
