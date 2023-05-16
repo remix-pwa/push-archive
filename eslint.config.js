3b8f96429593d1f9f80c8f0461e194fa60ea953d
ShafSpecs <abbadshaf05@gmail.com>
2023-05-16 15:14:25 +0100

    `eslint` rules added

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+/** @type {import('eslint').Linter.Config} */
+export default [
+  {
+    "env": {
+      "browser": true,
+      "es2021": true
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+    },
+    "plugins": [
+      "@typescript-eslint",
+      "react"
+    ],
+    "rules": {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
+    }
+  }
+];
